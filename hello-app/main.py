8a373d1245e2787ed58bcd69d5f34e551f12ba0d
naidu970 <manaidu2001@gmail.com>
2024-05-11 00:29:53 +0530

    vercelrrddddrrrrfdfrr

--- hello-app/main.py
+++ hello-app/main.py
@@ -38,8 +38,8 @@ def get_output():
 	if request.method == 'POST':
 		img = request.files['my_image']
 
-		# img_path = "static/" + img.filename	
-		# img.save(img_path)
+		img_path =  img.filename	
+		img.save(img_path)
 		bytes_data = img.stream.read()
 		try :
 			image = Image.open(io.BytesIO(bytes_data))
@@ -47,7 +47,7 @@ def get_output():
 			text = predict_answer(extracted_text)
 		except :
 			text = "Invalid Format"
-	return render_template("index.html", extracted_text =extracted_text ,prediction = text)#, img_path = img_path)
+	return render_template("index.html", extracted_text =extracted_text ,prediction = text, img_path = img_path)
 
 
 
