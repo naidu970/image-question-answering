ccb7d5e0ff6f637ec1d15f7b638f0b8384b6c558
naidu970 <manaidu2001@gmail.com>
2024-05-12 12:53:31 +0530

    added huggingface chat model

--- snapsearch_flask_app/main.py
+++ snapsearch_flask_app/main.py
@@ -56,13 +56,18 @@ def main():
 def about_page():
 	return "Naidu"
 
-@app.route("/process_edit", methods=['POST'])
+@app.route("/process_edit", methods=['GET','POST'])
 def process_edit():
-    uploaded_image_url = request.form['uploaded_image_url']
-    extracted_text = request.form['edited_text']
-    
-    text = predict_answer(extracted_text)
-    return render_template("index.html",  extracted_text =extracted_text ,uploaded_image_url=uploaded_image_url,prediction = text)
+	if True:
+		uploaded_image_url = request.form['uploaded_image_url']
+		extracted_text = request.form['edited_text']
+		try:
+			client = Client("https://qwen-qwen1-5-72b-chat.hf.space/--replicas/3kh1x/")
+			result = client.predict(extracted_text, [["Hello!", "null"]], "Hello!!", api_name="/model_chat")
+			text = result[1][1][1]
+		except:
+			text = predict_answer(extracted_text)
+	return render_template("index.html",  extracted_text =extracted_text ,uploaded_image_url=uploaded_image_url,prediction = text)
 
 
 @app.route("/submit", methods = ['GET', 'POST'])
